c8c6f2c3e1ba7917a4842949c93a6947f51e4f03
Misterobots <pause.not@gmail.com>
2019-10-22 10:17:54 -0400

    Update vegas.js

--- dist/vegas.js
+++ dist/vegas.js
@@ -277,7 +277,7 @@
 
             srcs.forEach(function (src) {
                // source = document.createElement('data');
-                source.src = src;
+             //   source.src = src;
 
                 video.appendChild(source);
                 video.data = src;
